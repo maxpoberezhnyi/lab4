04268d74dd9becd6e6a78eb06133523940a5e2f1
maxpoberezhnyi <mpobereznyj21@gmail.com>
2023-11-23 00:29:11 +0300

    5 лабораторна робота

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
-class Smartphone:
-    def __init__(self, name, release_year):
-        self.name = name
-        self.release_year = release_year
+def calculate_total_cost(file_path):
+    total = 0
 
-    def years_since_release(self, current_year):
-        return current_year - self.release_year
+    try:
+        with open(file_path, 'r', encoding='windows-1251') as file:
+            lines = file.readlines()
 
+            for line in lines:
+                separation = line.split('--')
 
-class Store:
-    def __init__(self):
-        self.smartphones = []
+                if len(separation) == 2:
+                    product_name = separation[0].strip()
+                    product_cost = float(separation[1].strip())  
 
-    def add_smartphone(self, smartphone):
-        self.smartphones.append(smartphone)
+                    total += product_cost
 
-    def remove_smartphones_before_year(self, year):
-        self.smartphones = [smartphone for smartphone in self.smartphones if smartphone.release_year >= year]
+    except FileNotFoundError:
+        print(f"Файл '{file_path}' не знайден")
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
 
-    def display_smartphones(self):
-        print("Наявні смартфони:")
-        for smartphone in self.smartphones:
-            print(f"{smartphone.name} ({smartphone.release_year}), {smartphone.years_since_release(2023)} роки з моменту виходу")
+    return total
 
+file_path = r'D:\lab4\lab.txt'
 
-if __name__ == "__main__":
-    iphone = Smartphone("iPhone 12", 2020)
-    samsung = Smartphone("Samsung Galaxy S21", 2021)
-    xiaomi = Smartphone("Xiaomi Mi 11", 2021)
-
-    phone_store = Store()
-
-    phone_store.add_smartphone(iphone)
-    phone_store.add_smartphone(samsung)
-    phone_store.add_smartphone(xiaomi)
-
-    phone_store.display_smartphones()
-
-    while True:
-        try:
-            year_to_filter = int(input("Введіть рік: "))
-            break
-        except ValueError:
-            print("Введіть ціле число.")
-
-    phone_store.remove_smartphones_before_year(year_to_filter)
-
-    phone_store.display_smartphones()
+total = calculate_total_cost(file_path)
+print("Загальна вартість товарів:", int(total))
